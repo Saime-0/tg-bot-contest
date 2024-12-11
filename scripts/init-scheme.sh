d7c46d5f09463ef716c7c90f2e21b69f294accdc
Saime Dejuwyu <acc.saime.d@gmail.com>
2024-12-12 00:51:35 +0300

    update scripts

--- scripts/init-scheme.sh
+++ scripts/init-scheme.sh
@@ -3,19 +3,15 @@
 # Завершать скрипт при ошибках
 set -e
 
-# Получение пути до директории с миграциями и названия файла БД из переменных окружения
-MIGRATION_DIR="${MIGRATION_DIR:-}" # Путь до директории с миграциями
-if [ -z "$MIGRATION_DIR" ]; then
-    echo "Ошибка: переменная окружения MIGRATION_DIR не установлена."
-    exit 1
-fi
-
 DSN="${DSN:-}" # Строка соединения с БД
 if [ -z "$DSN" ]; then
     echo "Ошибка: переменная окружения DSN не установлена."
     exit 1
 fi
 
+# Определить путь к директории с миграциями
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
+MIGRATION_DIR=${SCRIPT_DIR}/migrations/main
 
 last_file=$(ls -1 "$MIGRATION_DIR"/*up.sql | tail -n 1) # Получение названия последнего файла миграции
 db_version=$(basename "$last_file")  # Получаем имя файла без пути
