4b596dc4004e6dad1a144f35befcb3d7d7d755db
Saime Dejuwyu <acc.saime.d@gmail.com>
2024-12-12 03:09:10 +0300

    add logging

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"log/slog"
 	"os"
 	"os/signal"
 	"syscall"
--- internal/tg/client/loggingClient.go
+++ internal/tg/client/loggingClient.go
@@ -3,6 +3,7 @@ package client
 import (
 	"context"
 	"encoding/json"
+	"log/slog"
 	"net/http"
 
 	"github.com/PaulSonOfLars/gotgbot/v2"
@@ -14,6 +15,14 @@ type loggingClient struct {
 
 func (b loggingClient) RequestWithContext(ctx context.Context, token string, method string, params map[string]string, data map[string]gotgbot.FileReader, opts *gotgbot.RequestOpts) (json.RawMessage, error) {
 	rawMessage, err := b.BotClient.RequestWithContext(ctx, token, method, params, data, opts)
+
+	bb, err := json.MarshalIndent(rawMessage, "", "  ")
+	if err != nil {
+		return nil, err
+	}
+	if string(bb) != "[]" {
+		slog.Debug(string(bb))
+	}
 	return rawMessage, err
 }
 
