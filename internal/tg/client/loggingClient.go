d87ecda14f43c3975f51e238373a1b04234bb22a
Saime Dejuwyu <acc.saime.d@gmail.com>
2024-12-12 03:10:43 +0300

    remove logging

--- internal/tg/client/loggingClient.go
+++ internal/tg/client/loggingClient.go
@@ -3,7 +3,6 @@ package client
 import (
 	"context"
 	"encoding/json"
-	"log/slog"
 	"net/http"
 
 	"github.com/PaulSonOfLars/gotgbot/v2"
@@ -14,16 +13,7 @@ type loggingClient struct {
 }
 
 func (b loggingClient) RequestWithContext(ctx context.Context, token string, method string, params map[string]string, data map[string]gotgbot.FileReader, opts *gotgbot.RequestOpts) (json.RawMessage, error) {
-	rawMessage, err := b.BotClient.RequestWithContext(ctx, token, method, params, data, opts)
-
-	bb, err := json.MarshalIndent(rawMessage, "", "  ")
-	if err != nil {
-		return nil, err
-	}
-	if string(bb) != "[]" {
-		slog.Debug(string(bb))
-	}
-	return rawMessage, err
+	return b.BotClient.RequestWithContext(ctx, token, method, params, data, opts)
 }
 
 func newLoggingClient() loggingClient {
